fad1fee469c0090f7ae49f81335f39eaf31a0e7f
Fernando Silva <f.shiruba@gmail.com>
2024-05-07 15:55:55 -0300

    First Commit - Postgres working with Identity

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio_Backend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+
+    }
+    
+}
