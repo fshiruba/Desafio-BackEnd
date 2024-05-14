0903556b9dce28b4a44d21a6d8b034ee9561b7c3
Fernando Silva <f.shiruba@gmail.com>
2024-05-14 17:35:17 -0300

    Added business logic in AddBike

--- Desafio-BackEnd/Areas/Admin/Pages/AddBike.cshtml.cs
+++ Desafio-BackEnd/Areas/Admin/Pages/AddBike.cshtml.cs
@@ -1,4 +1,6 @@
 using Desafio_Backend.Models;
+using Desafio_Backend.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,18 +8,37 @@ namespace Desafio_Backend.Areas.Admin.Pages
 {
     public class AddBikeModel : PageModel
     {
+        public AddBikeModel(IAdminService adminService)
+        {
+            //UserManager = userManager;
+            AdminService = adminService;
+        }
+
         public string ReturnUrl { get; set; }
 
         [BindProperty]
         public Motorbike NewBike { get; set; }
 
-        public void OnGet()
-        {
+        //public UserManager<IdentityUser> UserManager { get; }
+        public IAdminService AdminService { get; }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {            
+            ReturnUrl = returnUrl;
         }
 
-        public async Task<object?> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync([FromQuery] string returnUrl = null)
         {
-            throw new NotImplementedException();
+            ReturnUrl = returnUrl;
+            //var user = await UserManager.GetUserAsync(HttpContext.User);
+            //NewBike.AdminId = user!.Id;
+
+            if (ModelState.IsValid)
+            {
+                await AdminService.AddMotorbike(NewBike);
+            }
+
+            return LocalRedirect(ReturnUrl);
         }
     }
 }
